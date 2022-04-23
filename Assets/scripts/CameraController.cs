e19e3d9bc46669e56008d494e254c9dd4af974e7
Denis Kotenko <d.kotenko@rj-games.com>
2022-04-23 16:52:35 +0300

    camera control

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,8 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _dragSpeed = 1;
+
+
     private InputSystem _input;
+    private bool _isDragging;
+    private Vector3 _cameraStartPos;
+    private Vector3 _dragStartPos;
+    private Vector3 _touchPosition;
+    
+    private Vector3 TouchPosition
+    {
+        get
+        {
+            if (!_input.Touches.Any())
+            {
+                return Vector3.zero;
+            }
+
+            _touchPosition.x = _input.Touches[0].position.x;
+            _touchPosition.y = _input.Touches[0].position.y;
+            _touchPosition.z = _camera.nearClipPlane;
+
+            return _touchPosition;
+        }
+    }
 
     private void Awake()
     {
@@ -10,11 +36,31 @@ public class CameraController : MonoBehaviour
     }
     private void Update()
     {
-        if (_input.HasTouches)
+        if (_input.Touches.Length == 1 && !_input.IsTouchOverGameObject)
         {
-            var touch = _input.Touches[0];
+            if (!_isDragging)
+            {
+                _dragStartPos = TouchPosition;
+                _cameraStartPos = transform.position;
+            }
 
-            Debug.Log($"[{GetType()}]: {touch.position}");
+            _isDragging = true;
         }
+
+        if (!_input.Touches.Any())
+        {
+            _isDragging = false;
+        }
+
+        if (!_isDragging)
+        {
+            return;
+        }
+
+        var startToWorld = _camera.ScreenToWorldPoint(_dragStartPos);
+        var mouseToWorld = _camera.ScreenToWorldPoint(TouchPosition);
+        var difference = (Vector2)(mouseToWorld - startToWorld);
+
+        transform.position = _cameraStartPos + (Vector3)difference * (_dragSpeed * -1);
     }
 }
